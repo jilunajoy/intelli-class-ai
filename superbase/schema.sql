c90d4d8f4bcaa99ca8563085f568d9d994b35143
jilunajoy <jilunajoy98@gmail.com>
2026-06-07 23:46:58 +0530

    Remove lesson_stats view definition from schema

---
+++ superbase/schema.sql
@@ -0,0 +1,30 @@
+[
+  {
+    "table_catalog": "postgres",
+    "table_schema": "public",
+    "table_name": "profiles",
+    "table_type": "BASE TABLE",
+    "self_referencing_column_name": null,
+    "reference_generation": null,
+    "user_defined_type_catalog": null,
+    "user_defined_type_schema": null,
+    "user_defined_type_name": null,
+    "is_insertable_into": "YES",
+    "is_typed": "NO",
+    "commit_action": null
+  },
+  {
+    "table_catalog": "postgres",
+    "table_schema": "public",
+    "table_name": "lessons",
+    "table_type": "BASE TABLE",
+    "self_referencing_column_name": null,
+    "reference_generation": null,
+    "user_defined_type_catalog": null,
+    "user_defined_type_schema": null,
+    "user_defined_type_name": null,
+    "is_insertable_into": "YES",
+    "is_typed": "NO",
+    "commit_action": null
+  }
+]
